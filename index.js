7af41560b98c273166f99dfca781d6dd84802b0f
Mutuyimana Alice <58625267+mutuyimana1@users.noreply.github.com>
2023-04-20 11:02:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ for (let i= 0; i > n.length; i++){
     console.log(n)
  }
 }
+//for question number 4 Alia it can't work it will display nothing ou know why? it because in our condition you set like i>n.length where if it is greater than there is 
+//no value bcz it will exceed our array
+// even you use the same approach but set your condition like this i<n.length it will display the initial array n.length times
+//try it in vscode and see
+
+//so you should do like this
+
+let n = [3, 5, 6, 7, 10, 11, 19, 20, 30];
+let newN = [];
+for (let i = 0; i < n.length; i++) {
+  if (n[i] > 10) {
+    newN.push(n[i]);
+  }
+}
+console.log(newN);
+
+//otherwise you did great 
